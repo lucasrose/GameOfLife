20fa0ace414ec5a60569775b152f3f6c46a157e7
unknown <lrose@ws-lrose.ccs.local>
2015-01-27 16:23:45 -0500

    Start to GameOfLife Kata

---
+++ GameOfLife/GameOfLife/GameOfLife.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class GameOfLife
+    {
+        public static Int32 AliveCell = 1;
+        public static Int32 DeadCell = 0;
+        Int32[,] grid;
+        Int32 lengthOfGrid;
+        Int32 heightOfGrid;
+
+
+        public GameOfLife(Int32 lengthOfGrid, Int32 heightOfGrid)
+        {
+            grid = new Int32 [lengthOfGrid, heightOfGrid];
+            this.lengthOfGrid = lengthOfGrid;
+            this.heightOfGrid = heightOfGrid;
+
+            SetupDeadCells(lengthOfGrid, heightOfGrid);
+            SetupLiveCells(lengthOfGrid, heightOfGrid);
+        }
+
+        private void SetupLiveCells(Int32 lengthOfGrid, Int32 heightOfGrid)
+        {
+            var midX = lengthOfGrid / 2;
+            var midY = heightOfGrid / 2;
+            grid[midX, midY] = AliveCell;
+            grid[midX, midY - 1] = AliveCell;
+            grid[midX + 1, midY] = AliveCell;
+            grid[midX + 1, midY + 1] = AliveCell;
+        }
+
+        private void SetupDeadCells(Int32 lengthOfGrid, Int32 heightOfGrid)
+        {
+
+            for (var cellX = 0; cellX < lengthOfGrid; cellX++)
+                for (var cellY = 0; cellY < lengthOfGrid; cellY++)
+                    grid[cellX, cellY] = DeadCell;
+               
+        }
+
+        public String GetCellValue(Int32 locationX, Int32 locationY)
+        {
+            if (grid[locationX, locationY] == 1)
+                return "Alive";
+            return "Dead";
+        }
+
+        private void KillCellWithLessThanTwoLiveNeighbors()
+        {
+            
+        }
+
+        private void SaveCellWithTwoOrThreeLiveNeighbors()
+        {
+
+        }
+
+        private void KillCellWithMoreThanThreeNeighbors()
+        {
+
+        }
+
+        private void SaveDeadCellWithThreeLiveNeighbors()
+        {
+
+        }
+    }
+}
---
+++ GameOfLife/GameOfLife/GameOfLifeTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLife
+{
+    [TestClass]
+    public class GameOfLifeTests
+    {
+        public static Int32 AliveCell = 1;
+        public static Int32 DeadCell = 0;
+        private GameOfLife ecosystem;
+
+        [TestInitialize]
+        public void testSetup()
+        {
+            ecosystem = new GameOfLife(4, 4);
+        }
+        [TestMethod]
+        public void TestCellIsAlive()
+        {
+            Assert.AreEqual("Alive", ecosystem.GetCellValue(2, 2));
+        }
+        [TestMethod]
+        public void TestCellIsDead()
+        {
+            Assert.AreEqual("Dead", ecosystem.GetCellValue(1, 1));
+        }
+        [TestMethod]
+        public void TestFewerThanTwoLiveNeighbors()
+        {
+            
+            Assert.IsTrue(false);
+        } 
+    }
+}
